be3f0bae5cef182f675d87881b7822efa1a6bd1d
shahjalal-mahmud <mahmud.nubtk@gmail.com>
2025-08-02 11:07:56 +0600

    Done final practice

---
+++ Traversal/finalPractice.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Node {
+public:
+    char data;
+    Node* left;
+    Node* right;
+    Node(char val) : data(val), left(nullptr), right(nullptr) {}
+};
+
+Node* buildPostorder(vector<char>& preorder, vector<char>& inorder, int inStart, int inEnd, int& preIndex, unordered_map<char, int>& inmap) {
+    if (inStart > inEnd) return nullptr;
+
+    char rootVal = preorder[preIndex++];
+    Node* root = new Node(rootVal);
+
+    int inRoot = inmap[rootVal]; 
+
+    root->left = buildPostorder(preorder, inorder, inStart, inRoot - 1, preIndex, inmap);
+    root->right = buildPostorder(preorder, inorder, inRoot + 1, inEnd, preIndex, inmap);
+
+    return root;
+}
+
+void postorder(Node* root) {
+    if (root == nullptr) return;
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+int main() {
+    vector<char> preorder = {'a','b','d','g','h','e','i','c','f','j'};
+    vector<char> inorder  = {'g','d','h','b','e','i','a','f','j','c'};
+
+    unordered_map<char, int> inmap;
+    for (int i = 0; i < inorder.size(); i++) {
+        inmap[inorder[i]] = i;
+    }
+
+    int preIndex = 0;
+    Node* root = buildPostorder(preorder, inorder, 0, inorder.size()-1, preIndex, inmap);
+
+    cout << "Postorder: ";
+    postorder(root);
+    return 0;
+}
